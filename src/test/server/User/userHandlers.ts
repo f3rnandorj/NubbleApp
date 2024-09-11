81ddf61705bb485d2317ee925238a246e8325988
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2024-09-11 12:02:43 -0300

    Aula 14 profile

--- src/test/server/User/userHandlers.ts
+++ src/test/server/User/userHandlers.ts
@@ -1,7 +1,9 @@
 import {BASE_URL, PageAPI} from '@api';
-import {UserAPI, USER_PATH} from '@domain';
 import {http, HttpResponse} from 'msw';
 
+import {UserAPI, USER_PATH} from '@domain';
+
+import {mockedPostResponse} from './postMocked';
 import {userMocked} from './userMocked';
 
 const FULL_URL = `${BASE_URL}${USER_PATH}`;
@@ -12,11 +14,20 @@ export const userHandlers = [
 
     return HttpResponse.json(response, {status: 200});
   }),
+
   http.get<{userId: string}>(`${FULL_URL}/:userId`, async ({params}) => {
     const userApi = userMocked.userList.find(
       user => user.id.toString() === params.userId,
     );
 
     return HttpResponse.json(userApi, {status: 200});
+  }),
+
+  http.get(`${BASE_URL}user/post`, async () => {
+    return HttpResponse.json(mockedPostResponse, {status: 200});
+  }),
+
+  http.get(`${BASE_URL}user/follow/is-following/:id`, async () => {
+    return HttpResponse.json({isFollowing: true}, {status: 200});
   }),
 ];
