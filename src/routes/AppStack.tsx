c76ebee9559eb4ef34770733fbd4cb326930394b
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2025-02-04 20:37:05 -0300

    Fluxo de push notification aula 18

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {NavigatorScreenParams} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {useNotificationAction} from '@services';
+import {useNotificationAction, usePermission} from '@services';
 
 import {useSaveNotificationToken} from '@domain';
 import {
@@ -54,6 +54,7 @@ interface Props {
 export function AppStack({initialRouteName = 'AppTabNavigator'}: Props) {
   useSaveNotificationToken();
   useNotificationAction();
+  usePermission('notification');
 
   return (
     <Stack.Navigator
